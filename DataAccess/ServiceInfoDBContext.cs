f465096aff77ba779d302575479202668418da72
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2020-03-25 10:38:47 +0100

    Comments, cleanup and project port

--- DataAccess/ServiceInfoDBContext.cs
+++ DataAccess/ServiceInfoDBContext.cs
@@ -1,14 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ServiceInfo.DataAccess
 {
     public class ServiceInfoDBContext : DbContext
     {
         public ServiceInfoDBContext(DbContextOptions<ServiceInfoDBContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Converts ServiceState enum to string for storing and converts it back when returning data.
+            modelBuilder
+                .Entity<ServiceInfo>()
+                .Property(s => s.ServiceState)
+                .HasConversion(
+                    x => x.ToString(),
+                    x => (ServiceInfo.State)Enum.Parse(typeof(ServiceInfo.State), x));
+        }
 
         public DbSet<ServiceInfo> ServiceInfos { get; set; }
     }
