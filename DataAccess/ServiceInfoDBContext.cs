694a80b94cef943454637bcfc3791ef3696f171f
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2020-03-25 10:03:17 +0100

    Update ServiceInfoDBContext.cs

--- DataAccess/ServiceInfoDBContext.cs
+++ DataAccess/ServiceInfoDBContext.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ServiceInfo.DataAccess
 {
-    public class ServiceInfoDBContext
+    public class ServiceInfoDBContext : DbContext
     {
+        public ServiceInfoDBContext() : base() { }
+
+        public DbSet<ServiceInfo> ServiceInfos { get; set; }
     }
 }
