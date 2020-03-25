82aac39e8e1532c5f871483964ce08af730ba42e
Bence <sifo996@gmail.com>
2020-03-25 09:53:03 +0100

    Entity done, Controller created

---
+++ Controller/ServiceInfoController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceInfo.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ServiceInfoController : ControllerBase
+    {
+        // GET: api/ServiceInfo
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/ServiceInfo/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ServiceInfo
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/ServiceInfo/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Entity/ServiceInfo.cs
@@ -0,0 +1,28 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceInfo
+{
+    public class ServiceInfo
+    {
+        [Flags]
+        public enum State
+        {
+            Running,
+            Paused
+        }
+        // Get or set a unique identifier for this service
+        public int Id { get; set; }
+
+        // Get or set Url address for this service
+        public String Url { get; set; }
+
+        // Get or set the priority of the service
+        public int Priority { get; set; }
+
+        // Get or set the state of the service
+        public State ServiceState { get; set; }
+    }
+}
